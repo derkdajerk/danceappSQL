e6e5ad7d2a2145b5acbdb1b308862f84424ff952
Derek Trauner <derektrauner@gmail.com>
2025-02-03 02:31:14 -0800

    Optimized execution time to < 10 seconds.

--- scrapeALLSQLreal.py
+++ scrapeALLSQLreal.py
@@ -1,10 +1,9 @@
 # took 17-20 seconds for just the 3 scrapes alone to run like this
-
 import mysql.connector
 import time
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.common.exceptions import NoSuchElementException
+from selenium.common.exceptions import NoSuchElementException, TimeoutException
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
@@ -26,27 +25,35 @@ def safe_find(element, xpath, default="N/A"):
     except NoSuchElementException:
         return default
 
-def scrape(url):
+def scrape(driver, url):
     start = time.time()
-    op = webdriver.ChromeOptions()
-    op.add_argument('headless')
-    op.page_load_strategy = 'eager'
-    op.add_argument('--log-level=3')
-    driver = webdriver.Chrome(options=op) # COMMENT OUT IF NEED TO FIND XCODE
+    print(f"\nScraping {url}")
     driver.get(url)
-    WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, '//div[contains(@class, "columns is-vcentered ClassTimeScheduleItemDesktop_separator__1vvuL")]')))
-    try: # Locate all class containers
+    
+    try:
+        WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, '//div[contains(@class, "columns is-vcentered ClassTimeScheduleItemDesktop_separator__1vvuL")]')))
+        print("Page loaded successfully")
+    except TimeoutException:
+        print("Timeout waiting for page load")
+        return []
+        
+    try:
         elements = driver.find_elements(By.XPATH, '//div[contains(@class, "columns is-vcentered ClassTimeScheduleItemDesktop_separator__1vvuL")]')
+        if len(elements) == 0:
+            print("WARNING: No class elements found")
+            return []
     except NoSuchElementException:
         print("Class containers not found")
-        elements = []
+        return []
+
     try:
         Date = driver.find_element(By.XPATH, '//h5[contains(@class, "is-marginless")]').text
         Date = Date[11:len(Date)]
     except NoSuchElementException:
+        print("Date not found")
         Date = "N/A"
+
     class_list = []
-    # Iterate over each class container
     for element in elements:
         className = safe_find(element, './/a[contains(@class, "ClassTimeScheduleItemDetails_classLink__1tyYz")]')  # Query within the parent container
         Instructor = safe_find(element, './/a[contains(@class, "ClassTimeScheduleItemDetails_link__1gju5")]')
@@ -67,37 +74,33 @@ def scrape(url):
     print("\nIt took", length, "seconds!")
     return class_list
 
-mycursor.execute("DELETE FROM mdc")
-mycursor.execute("ALTER TABLE mdc AUTO_INCREMENT = 1")
-mycursor.execute("DELETE FROM ml")
-mycursor.execute("ALTER TABLE ml AUTO_INCREMENT = 1")
-mycursor.execute("DELETE FROM tmilly")
-mycursor.execute("ALTER TABLE tmilly AUTO_INCREMENT = 1")
-mydb.commit()  # Commit the changes to the database
-tmilly = 'https://www.mindbodyonline.com/explore/locations/tmilly-studio'
-mL = 'https://www.mindbodyonline.com/explore/locations/movement-lifestyle-noho'
-mdc = 'https://www.mindbodyonline.com/explore/locations/millennium-dance-complex-studio-city'
+websites = [
+    ("https://www.mindbodyonline.com/explore/locations/tmilly-studio", "INSERT INTO tmilly (classname, instructor, price, time, length, date) VALUES (%s, %s, %s, %s, %s, %s)"),
+    ("https://www.mindbodyonline.com/explore/locations/movement-lifestyle-noho", "INSERT INTO ml (classname, instructor, price, time, length, date) VALUES (%s, %s, %s, %s, %s, %s)"),
+    ("https://www.mindbodyonline.com/explore/locations/millennium-dance-complex-studio-city", "INSERT INTO mdc (classname, instructor, price, time, length, date) VALUES (%s, %s, %s, %s, %s, %s)")
+]
 
-val = scrape(tmilly)
-val2 = scrape(mL)
-val3 = scrape(mdc)
-sql = "INSERT INTO tmilly (classname, instructor, price, time, length, date) VALUES (%s, %s, %s, %s, %s, %s)"
-sql2 = "INSERT INTO ml (classname, instructor, price, time, length, date) VALUES (%s, %s, %s, %s, %s, %s)"
-sql3 = "INSERT INTO mdc (classname, instructor, price, time, length, date) VALUES (%s, %s, %s, %s, %s, %s)"
-
-print("Entering data to SQL Tables:")
-mycursor.executemany(sql, val)
-print("Completed tmilly:")
-mycursor.executemany(sql2, val2)
-print("Completed ml:")
-mycursor.executemany(sql3, val3)
-print("Completed mdc:")
-
+for table in ["mdc", "ml", "tmilly"]:
+    mycursor.execute(f"DELETE FROM {table}")
+    mycursor.execute(f"ALTER TABLE {table} AUTO_INCREMENT = 1")
 mydb.commit()
 
-print(mycursor.rowcount, "was inserted.")
-    
+op = webdriver.ChromeOptions()
+op.add_argument('headless')
+op.page_load_strategy = 'none'
+op.add_argument('--log-level=3')
+driver = webdriver.Chrome(options=op)  # COMMENT OUT IF NEED TO FIND XCODE
+
+for url, sql in websites:
+    class_data = scrape(driver, url)
+    print(f"Entering data for {url}")
+    mycursor.executemany(sql, class_data)
+    print(f"Completed {url.split('/')[-1]}:")
+
+print("\nAll operations completed")
+
+print(f"{mycursor.rowcount} records inserted.")
+
+
 end = time.time()
-length = end - start
-# Show the results : this can be altered however you like
-print("\nIt took in total", length, "seconds!")
+print(f"\nTotal execution time: {end - start} seconds")
